004347984ff8829483da444b349a8500cb66caf8
Michael Bianchi <michaeldbianchi@gmail.com>
2018-04-29 20:11:01 -0500

    Init alias file

---
+++ config/fish/alias.fish
@@ -0,0 +1,12 @@
+# general aliases
+alias g git
+alias v vim
+alias cd.. 'cd ..'
+alias .. 'cd ..'
+alias ... 'cd ../..'
+alias .... 'cd ../../..'
+alias ..... 'cd ../../../..'
+
+alias md 'mkdir -p'
+alias cx 'chmod +x'
+alias 'c-x' 'chmod -x'

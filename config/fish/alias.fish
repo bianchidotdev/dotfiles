410b2419de133dd70efa69f972d4c6471d9641ba
Michael Bianchi <mbianchi@opploans.com>
2019-06-10 09:03:15 -0500

    Update dotfiles

--- config/fish/alias.fish
+++ config/fish/alias.fish
@@ -7,8 +7,22 @@ alias ... 'cd ../..'
 alias .... 'cd ../../..'
 alias ..... 'cd ../../../..'
 
+alias exa = 'exa -l --git'
+
 alias md 'mkdir -p'
 alias cx 'chmod +x'
 alias 'c-x' 'chmod -x'
 
 alias spacehogs 'open \'https://youtu.be/EmI77ZBeJrQi\''
+
+# Python
+alias venv = 'source venv/bin/activate'
+
+# Ruby
+alias rspec = 'bundle exec rspec spec'
+alias rs = 'rails s'
+alias rc = 'rails c'
+alias b = 'bundle install'
+
+# OppLoans
+alias awscreds 'python3 ~/.aws/onelogin/onelogin-aws-assume-role.py'
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,10 +1,13 @@
-set fish_user_paths $fish_user_paths ~/.cargo/bin
-
-source ~/.iterm2_shell_integration.(basename $SHELL)
+source ~/.iterm2_shell_integration.fish
 source ~/.config/fish/alias.fish
 
 # set -x GPG_TTY (tty)
 # set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 # gpgconf --launch gpg-agent
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-set -g fish_user_paths ~/.rbenv/shims $fish_user_paths
+
+set fish_user_paths /usr/local/bin $fish_user_paths
+set fish_user_paths "/usr/local/sbin" $fish_user_paths
+set fish_user_paths "$HOME/.cargo/bin" $fish_user_paths
+set fish_user_paths "$HOME/.rbenv/shims" $fish_user_paths
+
+set HOST 'localhost'
